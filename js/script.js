fe5db25cc5eb532ddcc4764132d4017ec52d3479
nathiely chaves <161589375+nathielychaves@users.noreply.github.com>
2024-08-20 07:52:43 -0300

    projeto do vetor

---
+++ js/script.js
@@ -0,0 +1,28 @@
+let vetor = [];
+
+function adicionarValor() {
+  const valor = Number(document.getElementById('valor').value);
+  vetor.push(valor);
+
+  const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+  const novalinha = tabela.insertRow();
+  const celula = novalinha.insertCell();
+  celula.textContent = valor;
+
+  document.getElementById('valor').value = '';
+    document.getElementById('valor').focus();
+}
+
+function mostrarPares() {
+  const tabela = document.getElementById("tabelaPares").getElementsByTagName("tbody")[0];
+
+
+  for (let i = 0; i < vetor.length; i++) {
+    const valor = vetor[i];
+    if (valor % 2 === 0) {
+      const linha = tabela.insertRow();
+      const celula = linha.insertCell();
+      celula.textContent = valor;
+    }
+  }
+}
